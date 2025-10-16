755ee27d6d01cf4c58e9e1beed425607aadc883d
aklemic <150902393+aklemic@users.noreply.github.com>
2025-10-16 22:06:35 +0200

    Add files via upload
    Projekt - baze podataka

---
+++ WebAppDB-dev/run_all.sql
@@ -0,0 +1,6 @@
+@C:\Users\Antonio\Desktop\WebAppDB-dev\ini.sql;
+@C:\Users\Antonio\Desktop\WebAppDB-dev\filter.sql;
+@C:\Users\Antonio\Desktop\WebAppDB-dev\dohvati.sql;
+@C:\Users\Antonio\Desktop\WebAppDB-dev\router.sql;
+@C:\Users\Antonio\Desktop\WebAppDB-dev\select.sql;
+@C:\Users\Antonio\Desktop\WebAppDB-dev\diu.sql;
